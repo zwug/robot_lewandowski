2b7113ed8fcfeaa2dcb461de9809c108e9d755a1
zwug <zwugme@gmail.com>
2022-02-13 19:46:04 +0300

    added date setting

--- src/googleSheets.ts
+++ src/googleSheets.ts
@@ -6,6 +6,8 @@ const auth = new google.auth.GoogleAuth({
   scopes: "https://www.googleapis.com/auth/spreadsheets",
 });
 
+const TWO_DAYS = 86400000 * 2;
+
 export const clearData = async (spreadsheetId: string) => {
   const client = await auth.getClient();
   const googleSheets = google.sheets({ version: "v4", auth: client });
@@ -16,6 +18,18 @@ export const clearData = async (spreadsheetId: string) => {
       auth,
       spreadsheetId,
       range: "Футбол с Островком!FB10:FB200",
+    });
+
+    let today = Date.now();
+    const matchDay = new Date(today + TWO_DAYS);
+
+    await googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      valueInputOption: 'USER_ENTERED',
+      range: "Футбол с Островком!FC7",
+      requestBody: {
+        values: [[`${matchDay.getDate()}.${matchDay.getMonth() + 1}.${matchDay.getFullYear()}`]]
+      }
     });
     console.log('The data is cleared');
   } catch (e) {
